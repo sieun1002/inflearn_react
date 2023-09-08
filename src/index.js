5e46a1fe2e8e0fb284699a72095287c0ae3d8e47
sieun1002 <nata20034@naver.com>
2023-09-09 07:30:01 +0900

    modified src/index.js -> add Clock Element and render it

--- src/index.js
+++ src/index.js
@@ -3,15 +3,28 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { createRoot } from "react-dom/client";
 
 import Library from "./ch03/Library";
+import Clock from "./ch04/Clock";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+//ch03
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Library />
   </React.StrictMode>
 );
+
+//ch04
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <Clock />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
