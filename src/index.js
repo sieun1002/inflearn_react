5d7c2e62c1a659ba891d19d2f0d0e0edef2f3a10
sieun1002 <nata20034@naver.com>
2023-09-11 19:23:51 +0900

    modified index.js for ch05

--- src/index.js
+++ src/index.js
@@ -7,24 +7,34 @@ import { createRoot } from "react-dom/client";
 
 import Library from "./ch03/Library";
 import Clock from "./ch04/Clock";
+import CommentList from "./ch05/CommentList";
 
-//ch03
+// //ch03
+// const root = createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <Library />
+//   </React.StrictMode>
+// );
+
+// //ch04
+// setInterval(() => {
+//   root.render(
+//     <React.StrictMode>
+//       <Clock />
+//     </React.StrictMode>,
+//     document.getElementById("root")
+//   );
+// }, 1000);
+
+//ch05
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Library />
-  </React.StrictMode>
+    <CommentList />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-//ch04
-setInterval(() => {
-  root.render(
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
