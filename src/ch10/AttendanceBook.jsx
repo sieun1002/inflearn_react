9fe4c3d33b307a11ccdd82b9c0c44d39348e9130
sieun1002 <nata20034@naver.com>
2023-09-14 23:57:16 +0900

    upload ch10

---
+++ src/ch10/AttendanceBook.jsx
@@ -0,0 +1,32 @@
+import Reat from "react";
+
+const students = [
+  {
+    id: 1,
+    name: "Inge",
+  },
+  {
+    id: 2,
+    name: "Steve",
+  },
+  {
+    id: 3,
+    name: "Bill",
+  },
+  {
+    id: 4,
+    name: "Jeff",
+  },
+];
+
+function AttendanceBook(props) {
+  return (
+    <ul>
+      {students.map((student) => {
+        return <li key={student.id}>{student.name}</li>;
+      })}
+    </ul>
+  );
+}
+
+export default AttendanceBook;
